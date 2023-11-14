f1ec801cc58f210fd0365ab74e590febe5398d22
Vrishabh <psvrishabh@gmail.com>
2023-11-15 00:05:01 +0530

    Adding tests (#2)
    * Adding tests

--- crates/wgpy_pyo3/wp_tests/test_array.py
+++ crates/wgpy_pyo3/wp_tests/test_array.py
@@ -1,6 +1,18 @@
 import webgpupy as wp
+import pytest
 
-def test_array():
-    arr = wp.array([[1], [2], [3]])
-    assert arr.shape == [3,1]
-    assert arr.flatten().tolist() == [1, 2, 3]
+@pytest.fixture
+def input_array():
+    return [[1.0], [2.0], [3.0], [4.0]]
+
+@pytest.fixture
+def wp_array(input_array):
+    return wp.array(input_array)
+
+def test_array(wp_array):
+    assert wp_array.shape == [4,1]
+    assert wp_array.flatten().tolist() == [1.0, 2.0, 3.0, 4.0]
+
+def test_array_astype(wp_array):
+    arr = wp_array.astype('uint8')
+    assert arr.flatten().tolist() == [1, 2, 3, 4]
